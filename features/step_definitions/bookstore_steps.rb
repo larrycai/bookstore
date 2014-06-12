799a5a7ec4df5f44b5f60cdf0a2a5488d155fb3b
larrycai <larry.caiyu@gmail.com>
2014-06-12 07:13:19 +0000

    change should to expect

--- features/step_definitions/bookstore_steps.rb
+++ features/step_definitions/bookstore_steps.rb
@@ -1,5 +1,4 @@
 def onlinebookstore(book_number,other_order_category,delivery_address)
-    print "hello"
     if (delivery_address == "gotland")
         return 1
     elsif (book_number <6)
@@ -29,10 +28,10 @@ Then /^order summary is (\d+)$/ do |fee|
   #print "heelo:", @book_number, fee
   
   #print real_fee
-  @real_fee.to_i.should == fee.to_i
+  expect(@real_fee.to_i).to eq (fee.to_i)
 end
 
 Then /^order summary is >(\d+)$/ do |fee|
   #print "heelo:", @real_fee.to_i," :", fee.to_i, "strange"
-  @real_fee.to_i.should > fee.to_i 
+  expect(@real_fee.to_i).to be > fee.to_i
 end
